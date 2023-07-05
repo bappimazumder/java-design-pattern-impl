454552117c1b1a6ec6f90ab74d4d454ba38fa385
Bappi Mazumder <mazumderbappi@gmail.com>
2023-07-05 18:47:10 +0600

    Implement double locking Thread Safe singleton

--- src/main/java/com/bappi/designPattern/creational/singleton/ThreadSafeSingleton.java
+++ src/main/java/com/bappi/designPattern/creational/singleton/ThreadSafeSingleton.java
@@ -25,4 +25,25 @@ public class ThreadSafeSingleton {
         }
         return instance;
     }
+
+    /*
+    * The preceding implementation works fine and provides thread-safety,
+    * but it reduces the performance because of the cost associated with the synchronized method,
+    * although we need it only for the first few threads that might create separate instances.
+    * To avoid this extra overhead every time, double-checked locking principle is used.
+    * In this approach, the synchronized block is used inside the if condition with an additional
+    * check to ensure that only one instance of a singleton class is created.
+    * */
+    public static ThreadSafeSingleton getInstanceUsingDoubleLocking(){
+        if(instance == null){
+            synchronized (ThreadSafeSingleton.class){
+                if (instance == null){
+                    instance = new ThreadSafeSingleton();
+                }
+            }
+
+        }
+        return instance;
+    }
+
 }
